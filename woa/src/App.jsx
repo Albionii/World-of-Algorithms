cd3294a2e805533262c26bcd0b4e28fa344c4a83
Albion Qerreti <albionqerreti221b@gmail.com>
2024-10-21 08:41:29 +0200

    First Page Display

---
+++ woa/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>What would you like to do, Sire?</h1>
+      <div className='flex flex-col gap-2 m-10'>
+        <button onClick={""}>Generate Tree</button>
+        <button onClick={""}>Dijkstra Algorithm</button>
+        <button onClick={""}>A*</button>
+        <button onClick={""}>Depth First Search</button>
+        <button onClick={""}>Breadth First Search</button>
+      </div>
+    </>
+  )
+}
+
+export default App
